316c16154a11cb4e921eacf25aa12dd9fc026f15
ubaidraza-06 <ubaidraza6465@gmail.com>
2025-08-02 13:19:10 +0500

    final code pushed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -60,6 +60,13 @@ const Header = () => {
                 {isHomeDropdownOpen && (
                   <div className="absolute top-full left-0 mt-2 w-48 bg-white text-black rounded shadow-lg z-[999]">
                     <Link
+                      to="/"
+                      className="block px-4 py-2 hover:bg-gray-100 transition-colors duration-200"
+                      onClick={() => setIsHomeDropdownOpen(false)}
+                    >
+                      Home Page One
+                    </Link>
+                    <Link
                       to="/homepage-two"
                       className="block px-4 py-2 hover:bg-gray-100 transition-colors duration-200"
                       onClick={() => setIsHomeDropdownOpen(false)}
